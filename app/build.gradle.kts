648c4511602ca7166b8c55722f7bc5eb7f79db5e
Nikita <devnikb@gmail.com>
2024-11-20 10:00:58 +0500

    Revert "R8"
    This reverts commit 77750781218c98e605cf02d622af5bd49eb8ccd6.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,7 +34,7 @@ android {
 
     buildTypes {
         release {
-            isMinifyEnabled = true
+            isMinifyEnabled = false
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
@@ -60,14 +60,6 @@ android {
         packaging {
             resources {
                 excludes += "/META-INF/{AL2.0,LGPL2.1}"
-                excludes += "/okhttp3/internal/publicsuffix/NOTICE"
-                excludes += "/kotlin/**"
-                excludes += "META-INF/androidx.*.version"
-                excludes += "META-INF/com.google.*.version"
-                excludes += "META-INF/kotlinx_*.version"
-                excludes += "kotlin-tooling-metadata.json"
-                excludes += "DebugProbesKt.bin"
-                excludes += "META-INF/com/android/build/gradle/*"
             }
         }
     }
